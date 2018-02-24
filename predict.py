1d7f06188bcb6bf6f1eb7739e858d217aa56b767
PauBatlle <paubatlle11@outlook.com>
2018-02-24 04:09:36 +0100

    predict 0.1

--- predict.py
+++ predict.py
@@ -6,6 +6,15 @@ parser.add_argument("--body", required = True)
 args_parsed = parser.parse_args()
 
 args_parsed.body
-
 args_parsed.title
-print(args_parsed)
+
+with open(args_parsed.title, 'r') as f:
+	title = f.readlines()
+
+with open(args_parsed.body, 'r') as f:
+	body = f.readlines()
+
+#Read models
+
+#Return predictions 
+
